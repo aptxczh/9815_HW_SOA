32bdeeae80a00cf80007eaced8d950da9ab23dc3
Alex Chen <zhihaochengsm@163.com>
2016-11-21 16:33:11 -0500

    Add files via upload

---
+++ p2-3/productservice.hpp
@@ -0,0 +1,212 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+///                                 Avinash Oza & Zhihao Chen
+///////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * productservice.hpp defines Bond, IRSwap and Futures ProductServices
+ */
+
+#include <iostream>
+#include <map>
+#include "products.hpp"
+#include "soa.hpp"
+
+/**
+ * Bond Product Service to own reference data over a set of bond securities.
+ * Key is the productId string, value is a Bond.
+ */
+class BondProductService : public Service<string,Bond>
+{
+
+public:
+  // BondProductService ctor
+  BondProductService();
+
+  // Return the bond data for a particular bond product identifier
+  Bond& GetData(string productId);
+
+  // Add a bond to the service (convenience method)
+  void Add(Bond &bond);
+
+  // Get all Bonds with the specified ticker
+  vector<Bond> GetBonds(string& _ticker);
+private:
+  map<string,Bond> bondMap; // cache of bond products
+
+};
+
+/**
+ * Interest Rate Swap Product Service to own reference data over a set of IR Swap products
+ * Key is the productId string, value is a IRSwap.
+ */
+class IRSwapProductService : public Service<string,IRSwap>
+{
+public:
+  // IRSwapProductService ctor
+  IRSwapProductService();
+
+  // Return the IR Swap data for a particular bond product identifier
+  IRSwap& GetData(string productId);
+
+  // Add a bond to the service (convenience method)
+  void Add(IRSwap &swap);
+  // Get all Swaps with the specified fixed leg day count convention
+  vector<IRSwap> GetSwaps(DayCountConvention _fixedLegDayCountConvention);
+
+  // Get all Swaps with the specified fixed leg payment frequency
+  vector<IRSwap> GetSwaps(PaymentFrequency _fixedLegPaymentFrequency);
+
+  // Get all Swaps with the specified floating index
+  vector<IRSwap> GetSwaps(FloatingIndex _floatingIndex);
+
+  // Get all Swaps with a term in years greater than the specified value
+  vector<IRSwap> GetSwapsGreaterThan(int _termYears);
+
+  // Get all Swaps with a term in years less than the specified value
+  vector<IRSwap> GetSwapsLessThan(int _termYears);
+
+  // Get all Swaps with the specified swap type
+  vector<IRSwap> GetSwaps(SwapType _swapType);
+
+  // Get all Swaps with the specified swap leg type
+  vector<IRSwap> GetSwaps(SwapLegType _swapLegType);
+
+private:
+  map<string,IRSwap> swapMap; // cache of IR Swap products
+
+};
+
+/**
+* Futures Product Service to own reference data over a set of IR Swap products
+* Key is the productId string, value is a IRSwap.
+*/
+class FutureProductService : public Service<string, Future>
+{
+
+public:
+	// FutureProductService ctor
+	FutureProductService();
+
+	// Return the pointer to Future data for a particular future product identifier
+	Future& GetData(string productId);
+
+	// Add a future to the service (convenience method)
+	void Add(Future& future);
+
+private:
+	map<string, Future> futureMap; // cache of future products
+};
+
+
+
+
+BondProductService::BondProductService()
+{
+  bondMap = map<string,Bond>();
+}
+
+Bond& BondProductService::GetData(string productId)
+{
+  return bondMap[productId];
+}
+
+void BondProductService::Add(Bond &bond)
+{
+  bondMap.insert(pair<string,Bond>(bond.GetProductId(), bond));
+}
+
+vector<Bond> BondProductService::GetBonds(string& _ticker) {
+	vector<Bond> res;
+	for (auto i : bondMap) {
+		if (i.second.GetTicker() == _ticker)
+			res.push_back(i.second);
+	}
+	return res;
+}
+
+IRSwapProductService::IRSwapProductService()
+{
+  swapMap = map<string,IRSwap>();
+}
+
+IRSwap& IRSwapProductService::GetData(string productId)
+{
+  return swapMap[productId];
+}
+
+void IRSwapProductService::Add(IRSwap &swap)
+{
+  swapMap.insert(pair<string,IRSwap>(swap.GetProductId(), swap));
+}
+
+vector<IRSwap> IRSwapProductService::GetSwaps(DayCountConvention _fixedLegDayCountConvention) {
+	vector<IRSwap> res;
+	for (auto i : swapMap) {
+		if (i.second.GetFixedLegDayCountConvention() == _fixedLegDayCountConvention)
+			res.push_back(i.second);
+	}
+	return res;
+}
+vector<IRSwap> IRSwapProductService::GetSwaps(PaymentFrequency _fixedLegPaymentFrequency) {
+	vector<IRSwap> res;
+	for (auto i : swapMap) {
+		if (i.second.GetFixedLegPaymentFrequency() == _fixedLegPaymentFrequency)
+			res.push_back(i.second);
+	}
+	return res;
+}
+vector<IRSwap> IRSwapProductService::GetSwaps(FloatingIndex _floatingIndex) {
+	vector<IRSwap> res;
+	for (auto i : swapMap) {
+		if (i.second.GetFloatingIndex() == _floatingIndex)
+			res.push_back(i.second);
+	}
+	return res;
+}
+vector<IRSwap> IRSwapProductService::GetSwapsGreaterThan(int _termYears){
+	vector<IRSwap> res;
+	for (auto i : swapMap) {
+		if (i.second.GetTermYears() > _termYears)
+			res.push_back(i.second);
+	}
+	return res;
+}
+vector<IRSwap> IRSwapProductService::GetSwapsLessThan(int _termYears){
+	vector<IRSwap> res;
+	for (auto i : swapMap) {
+		if (i.second.GetTermYears() < _termYears)
+			res.push_back(i.second);
+	}
+	return res;
+}
+vector<IRSwap> IRSwapProductService::GetSwaps(SwapType _swapType) {
+	vector<IRSwap> res;
+	for (auto i : swapMap) {
+		if (i.second.GetSwapType() == _swapType)
+			res.push_back(i.second);
+	}
+	return res;
+}
+vector<IRSwap> IRSwapProductService::GetSwaps(SwapLegType _swapLegType) {
+	vector<IRSwap> res;
+	for (auto i : swapMap) {
+		if (i.second.GetSwapLegType() ==_swapLegType)
+			res.push_back(i.second);
+	}
+	return res;
+}
+FutureProductService::FutureProductService()
+{
+	futureMap = map<string, Future>();
+}
+
+Future& FutureProductService::GetData(string productId)
+{
+	return futureMap[productId];
+}
+
+void FutureProductService::Add(Future& future)
+{
+	futureMap.insert(pair<string, Future>(future.GetProductId(), future));
+}
+
+
---
+++ p2-3/test_products.cpp
@@ -0,0 +1,97 @@
+///////////////////////////////////////////////////////////////////////////////////////////
+///                                 Avinash Oza & Zhihao Chen
+///////////////////////////////////////////////////////////////////////////////////////////
+/**
+ * test program for our ProductServices
+ */
+
+#include <iostream>
+#include <vector>
+#include "products.hpp"
+#include "productservice.hpp"
+
+using namespace std;
+template<typename T>
+void print(const vector<T>& v)
+{//help function to print out result of problem 3
+	for (auto i : v)
+		cout << i << endl;
+	cout << endl;
+}
+
+int main()
+{ ////////////////////////
+  /// Test Problem 2
+  ////////////////////////
+
+  // Create the 3 months EuroDollarfuture issued by China;
+  date maturityDate(2017, Mar, 16);
+  string ed1 = "912828ONE";
+  EDFuture f1(ed1, "China","EDH7", maturityDate);
+  // Create the 6 months EuroDollarfuture
+  date maturityDate2(2017, Jun, 16);
+  string ed2 = "912828TW0";
+  Future f2(ed2, EuroDollarFuture, Cash, "EDM7", maturityDate2);
+
+
+  // Create the 10Y treasury note
+  date maturityDateBond(2026, Nov, 16);
+  string cusip = "912828M56";
+  Bond treasuryBond(cusip, CUSIP, "T", 2.25, maturityDateBond);
+
+  // Create the 10Y treasury note future;
+  date maturityDate3(2017, Sep, 16);
+  string bd = "912829ONE";
+  BDFuture f3(bd, treasuryBond, "TYU7", maturityDate3);
+
+  // Create a FutureProductService
+  FutureProductService *futureProductService = new FutureProductService();
+
+  // Add the 10Y note to the BondProductService and retrieve it from the service
+  futureProductService->Add(f1);
+  futureProductService->Add(f2);
+  futureProductService->Add(f3);
+  Future& future1 = futureProductService->GetData(ed1);
+  Future& future2 = futureProductService->GetData(ed2);
+  Future& future3 = futureProductService->GetData(bd);
+  cout << "Future: " << future1.GetProductId() << " ==> "; future1.print();
+  cout << "Future: " << future2.GetProductId() << " ==> "; future2.print();
+  cout << "Future: " << future3.GetProductId() << " ==> "; future3.print();
+
+  ////////////////////////
+  /// Test Problem 3
+  ////////////////////////
+  // Create the Spot 10Y Outright Swap
+  cout << endl << "**************************Test Problem 3******************************************\n";
+  date effectiveDate(2016, Nov, 16);
+  date terminationDate(2026, Nov, 16);
+  string outright10Y = "Spot-Outright-10Y";
+  IRSwap outright10YSwap(outright10Y, THIRTY_THREE_SIXTY, THIRTY_THREE_SIXTY, SEMI_ANNUAL, LIBOR, TENOR_3M, effectiveDate, terminationDate, USD, 10, SPOT, OUTRIGHT);
+
+  // Create the IMM 2Y Outright Swap
+  date effectiveDate2(2016, Dec, 20);
+  date terminationDate2(2018, Dec, 20);
+  string imm2Y = "IMM-Outright-2Y";
+  IRSwap imm2YSwap(imm2Y, THIRTY_THREE_SIXTY, THIRTY_THREE_SIXTY, SEMI_ANNUAL, LIBOR, TENOR_3M, effectiveDate2, terminationDate2, USD, 2, IMM, OUTRIGHT);
+  // Create the IMM 3M Outright Swap
+  date effectiveDate3(2016, Dec, 20);
+  date terminationDate3(2017, Dec, 20);
+  string imm1Y = "IMM-Outright-1Y";
+  IRSwap imm1YSwap(imm1Y, THIRTY_THREE_SIXTY, THIRTY_THREE_SIXTY, SEMI_ANNUAL, LIBOR, TENOR_3M, effectiveDate3, terminationDate3, USD, 1, IMM, OUTRIGHT);
+  // Create a IRSwapProductService
+  IRSwapProductService *swapProductService = new IRSwapProductService();
+
+  // Add the Spot 10Y Outright Swap to the IRSwapProductService and retrieve it from the service
+  swapProductService->Add(outright10YSwap);
+
+  // Add the IMM 2Y Outright Swap to the IRSwapProductService and retrieve it from the service
+  swapProductService->Add(imm2YSwap);
+  // Add the IMM 3m Outright Swap to the IRSwapProductService and retrieve it from the service
+  swapProductService->Add(imm1YSwap);
+  cout << "Term Test: Greater Than 2 years\n";
+  print(swapProductService->GetSwapsGreaterThan(2));
+  cout << "Term Test: Less Than 2 years\n";
+  print(swapProductService->GetSwapsLessThan(2));
+  cout << "Get Swap Test: \n";
+  print(swapProductService->GetSwaps(IMM));
+}
