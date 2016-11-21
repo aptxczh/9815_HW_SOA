940d32f804c0d5cbbe09e2b8b1ca30fc1b7da6e4
Avinash Oza <avioza@gmail.com>
2016-11-20 20:51:34 -0500

    Update p1 file, readme and cMake file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 9815_HW_SOA
+
+
+* p1 directory:
+    * To run the code for part 1, edit CMakeLists.txt with the proper path to your boost libraries. Run cmake . && make. Then you can run "./p1" to set the value of the process, or "./p1 1" to run the child process. It will display the value if it exists, otherwise print a message saying there is no value in the shared memory space.
--- p1/test_interprocess.cpp
+++ p1/test_interprocess.cpp
@@ -1,22 +1,35 @@
-#include <boost/interprocess/managed_shared_memory.hpp> 
-#include <iostream> 
+///////////////////////////////////////////////////////////////////////////////////////////
+///                                 Avinash Oza & Zhihao Chen
+///////////////////////////////////////////////////////////////////////////////////////////
+// Demonstrates the usage of boost inter process to store and retrieve and integer
 
-int main()
+#include <boost/interprocess/managed_shared_memory.hpp>
+#include <iostream>
+using std::cout;
+
+int main(int argc, char ** argv)
 {
-    //if pass not arguments, run the parent process; if pass no fewer than 1 argument, run the child process
-    //Run Parent Process first in order to set the interger. Then run child process to modify and output it.
-    int argc =1;
+    // If no parameters are passed, run the parent process. If any argument is passed, run the child process.
 	if (argc == 1) {//Parent Process
-		boost::interprocess::shared_memory_object::remove("Shared_Memory");//Remove shared_memory
 		boost::interprocess::managed_shared_memory managed_shm(boost::interprocess::open_or_create, "Shared_Memory", 1024);
 		int *i = managed_shm.find_or_construct<int>("Integer")(99);
-		std::cout << *i << std::endl;
+		std::cout << "Parent process integer value: " << *i << std::endl;
 	}
-	else {//Child Process
+	else {
+        //Child Process
 		boost::interprocess::managed_shared_memory managed_shm2(boost::interprocess::open_or_create, "Shared_Memory", 1024);
 		std::pair<int*, std::size_t> p = managed_shm2.find<int>("Integer");
-		if (p.first)//if the shared object with name "Integer" exists
-			std::cout << (*p.first)+1 << std::endl;//modify and output it
+		if (p.first) //if the shared object with name "Integer" exists
+        {
+            std::cout << "Child process integer value + 1: "  <<  (*p.first)+1 << std::endl; //modify and output it
+            boost::interprocess::shared_memory_object::remove("Shared_Memory");//Remove shared_memory
+
+        }
+        else {
+            // show a message letting the user know that nothing is stored.
+            std:cout << "There appears to be no value stored " << std::endl;
+        }
 	}
+
 	return 0;
 }
